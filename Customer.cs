d285eed8ba9a704d067f41f2444f9a6610f2ffe6
Jonathan Ludvigsson <114058170+JonathanLudvigsson@users.noreply.github.com>
2022-11-22 10:14:02 +0100

    Added customer class which derives from user class

---
+++ Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KiwiBankomaten
+{
+    internal class Customer : User
+    {
+        Console.WriteLine("Hello from the customer class");
+    }
+}
