d1845e56a5648a3d604ddda82e184e609a1b6285
Jonathan Ludvigsson <114058170+JonathanLudvigsson@users.noreply.github.com>
2022-11-22 13:27:44 +0100

    Minor fix to match UML

--- Customer.cs
+++ Customer.cs
@@ -6,10 +6,10 @@ namespace KiwiBankomaten
 {
     internal class Customer : User
     {
-        List<Accounts> Accounts;
+        private List<Accounts> Accounts;
         public Customer(int id, string username, string password, List<Accounts> accList)
         {
-            ID = id;
+            Id = id;
             UserName = username;
             Password = password;
             Accounts = accList;
