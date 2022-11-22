5ed94540a8d3ac281c0e3678339e6e7a074fc64a
Max Dahlberg <max.dahlberg1@edu.varberg.se>
2022-11-22 13:41:02 +0100

    Added fields to User

--- User.cs
+++ User.cs
@@ -10,5 +10,38 @@ namespace KiwiBankomaten
         private string _password;
         private int _id;
 
+        public string UserName
+        { 
+            get 
+            { 
+                return this._userName;
+            }
+            set
+            {
+                this._userName = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                this._password = value;
+            }
+        }
+        public int Id
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                this._id = value;
+            }
+        }
     }
 }
