04c138eed661c9e27ee771ce39ca96d8b60e8353
Charlie Skibsted <charlieskibsted@gmail.com>
2022-11-29 11:09:40 +0100

    Created new features

--- BankAccount.cs
+++ BankAccount.cs
@@ -6,17 +6,21 @@ namespace KiwiBankomaten
 {
     internal class BankAccount
     {
+        private int _accountNumber;
         private string _accountName;
         private decimal _amount;
         private string _currency;
         private decimal _interest;
         private List<string> _log;
 
+        public int AccountNumber { get => _accountNumber; }
         public string AccountName {get => _accountName;}
         public decimal Amount { get => _amount; set => _amount = value; }
         public string Currency { get => _currency; set => _currency = value; }
         public decimal Interest { get => _interest; set => _interest = value; }
-
+        
+        private static int AccountNumberCounter = 40448653;
+        
         // For testing, to give value to accounts
         public BankAccount(string _accountName, decimal _value, string _currency,
             decimal _interest)
@@ -25,6 +29,8 @@ namespace KiwiBankomaten
             this._amount = _value;
             this._currency = _currency;
             this._interest = _interest;
+            _accountNumber = AccountNumberCounter;
+            AccountNumberCounter++;
         }
 
         // Use this constructor for users
@@ -34,6 +40,8 @@ namespace KiwiBankomaten
             _amount = 0;
             this._currency = _currency;
             this._interest = _interest;
+            _accountNumber = AccountNumberCounter;
+            AccountNumberCounter++;
         }
     }
 }
