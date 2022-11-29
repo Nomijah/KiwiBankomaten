1317ad5c1fdeae14fe0205e6500ee0ce5d4f89b0
Max Dahlberg <max.dahlberg1@edu.varberg.se>
2022-11-29 12:34:45 +0100

    Pretty

--- Customer.cs
+++ Customer.cs
@@ -64,7 +64,7 @@ namespace KiwiBankomaten
             int index = 1;
             foreach (var item in BankAccounts.Values)
             {
-                Console.WriteLine($"-{index}) -\tKontoNamn : {item.AccountName} -\tKontoSaldo : {item.Amount} {item.Currency}");
+                Console.WriteLine($"-{index}) -\tKontoNamn : {item.AccountName} - KontoSaldo : {item.Amount} {item.Currency}");
                 index++;
             }
         }
