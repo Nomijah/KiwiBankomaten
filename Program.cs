4d6bc760849bea7ed61ac069c382336635d7b2a2
Max Dahlberg <max.dahlberg1@edu.varberg.se>
2022-11-30 11:01:07 +0100

    Commented on Everyting in program class (IsValueNumberCheck() and PressEnterToContinue())

--- Program.cs
+++ Program.cs
@@ -89,7 +89,7 @@ namespace KiwiBankomaten
 
         public static void LogOut()
         {
-            RunProgram();
+            RunProgram(); // Makes the program go back to the log in menu
         }
 
         public static void CustomerMenu(int userKey)
@@ -106,10 +106,10 @@ namespace KiwiBankomaten
                 switch (choice)
                 {
                     case "1":
-                        obj.AccountOverview();
+                        obj.AccountOverview(); // Overviews the Accounts and their respective balances
                         break;
                     case "2":
-                        obj.TransferBetweenCustomerAccounts();
+                        obj.TransferBetweenCustomerAccounts(); // Transfers a value between two accounts the user possesses
                         break;
                     case "3":
 
@@ -130,53 +130,56 @@ namespace KiwiBankomaten
                 Console.Clear();// clearing console, 
             } while (true);
         }
-        public static void IsValueNumberCheck(out decimal amountMoney, bool isValueNumberCheck)
+       
+        public static void IsValueNumberCheck(out decimal amountMoney, bool isValueNumberCheck) // Checks whether or not "decimal amountMoney" is valid input
         {
+            // amountMoney == The money that is to be transfered and recieved // isValueNumberCheck == If the user input is correct
             do
             {
-                if (decimal.TryParse(Console.ReadLine(), out amountMoney) && amountMoney > 0)
+                if (decimal.TryParse(Console.ReadLine(), out amountMoney) && amountMoney > 0) // Gets user input => Checks if its a decimal => Checks if its larger than 0
                 {
-                    isValueNumberCheck = false;
+                    isValueNumberCheck = false; // If amountMoney is a valid input
                 }
-                else if (amountMoney < 0)
+                else if (amountMoney < 0) // Checks if amountMoney is larger than 0
                 {
                     Console.WriteLine("Input has to be positive and cannot be 0. Please Try Again!");
-                    isValueNumberCheck = true;
+                    isValueNumberCheck = true; // If amountMoney is lower or equal to 0
                 }
-                else
+                else // If the answer is not a number or other invalid input
                 {
                     Console.WriteLine("Please input a Number!");
-                    isValueNumberCheck = true;
+                    isValueNumberCheck = true; // If amountMoney is an invalid input
                 }
 
             } while (isValueNumberCheck);
         }
-        public static void IsValueNumberCheck(out int transferFromOrToWhichAccount, int minValue, int maxValue, bool isValueNumberCheck)
+        public static void IsValueNumberCheck(out int transferFromOrToWhichAccount, int minValue, int maxValue, bool isValueNumberCheck) // Checks whether or not "int transferFromOrToWhichAccount" is valid input
         {
+            // transferFromOrToWhichAccount == The account from which the money will be removed or added // minValue == The key for the Account at the top of the dictionary // maxValue == The key for the Account at the bottom of the dictionary // isValueNumberCheck == If the user input is correct
             do
             {
-                if (int.TryParse(Console.ReadLine(), out transferFromOrToWhichAccount) && minValue <= transferFromOrToWhichAccount && maxValue >= transferFromOrToWhichAccount)
+                if (int.TryParse(Console.ReadLine(), out transferFromOrToWhichAccount) && minValue <= transferFromOrToWhichAccount && maxValue >= transferFromOrToWhichAccount) // Gets user input => Checks if its a decimal => Checks if it's in the set range
                 {
-                    isValueNumberCheck = false;
+                    isValueNumberCheck = false; // If transferFromOrToWhichAccount is a valid input
                 }
-                else if (minValue > transferFromOrToWhichAccount || maxValue < transferFromOrToWhichAccount)
+                else if (minValue > transferFromOrToWhichAccount || maxValue < transferFromOrToWhichAccount) // Checks if transferFromOrToWhichAccount is in the given range 
                 {
                     Console.WriteLine("Please input a Number between {0} and {1}!", minValue, maxValue);
-                    isValueNumberCheck = true;
+                    isValueNumberCheck = true; // If transferFromOrToWhichAccount is an invalid input
                 }
-                else
+                else // If the answer is not a number or other invalid input
                 {
                     Console.WriteLine("Please input a Number!");
-                    isValueNumberCheck = true;
+                    isValueNumberCheck = true; // If transferFromOrToWhichAccount is an invalid input
                 }
 
-            } while (isValueNumberCheck);
+            } while (isValueNumberCheck); // Loops if input is invalid
         }
-        public static void PressEnterToContinue()
+        public static void PressEnterToContinue() // Stops the program until the user presses "Enter"
         {
             Console.WriteLine("Klicka enter för att komma till huvudmenyn");
-            ConsoleKey enterPressed = Console.ReadKey(true).Key;
-            while (!Console.KeyAvailable && enterPressed != ConsoleKey.Enter)
+            ConsoleKey enterPressed = Console.ReadKey(true).Key; // Gets the input from the user
+            while (!Console.KeyAvailable && enterPressed != ConsoleKey.Enter) // Loops if the user Presses any button other than "Enter"
             {
                 enterPressed = Console.ReadKey(true).Key;
             }
