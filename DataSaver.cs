7af78dd6ad09e1f258293012829ade08b8c1ef85
DanielDarwiche <danielinti1@hotmail.com>
2022-12-08 16:27:04 +0100

    Touches

--- DataSaver.cs
+++ DataSaver.cs
@@ -124,43 +124,19 @@ namespace KiwiBankomaten
         //Problemet med 1 textfil är att det är text och inte objekt, så det blir svårt att koppla ihop vilken text som tillhör vilken
         //problemet med flera textfiler är att det blir svårt att koppla ihop? eller för många filer
 
+        //Tasks:
+        // - primary
         //    X    Make DataSaver possible for other files than File1
         //    X    Connect the "DataBase"-Files to each other.
         //    X    hur välförståeligt behöver detta vara för admin/användare?
 
-
-
+        // - secondary
         //    X    Implement DataReading Method to admin menu, to see files and read them     när den funkar fullständigt
         //    X    Make DataSaver not public to the user, only workable       efter all testning, när det funkar
         //    X    Implement DataSaver method in program so that it will sync files during changes and when pressing exit
-        //på passande platser osv.
+        //         på passande platser osv.
 
-
-        // Method to read and show data from chosen file.
-        //DataSaver.DataReading("File1");  HOW TO USE
-        public static void DataReading(string name)
-        {
-            // Takes in parameter which is the name of the file. Example, write "File1" 
-            string choice = @"C:\Users\danie\Desktop\" + name + ".txt";
-            StreamReader sr = new StreamReader($"{choice}");
-            Console.WriteLine($"Content of the File ({name}): \n");
-
-            // This is use to specify from where to start reading input stream
-            sr.BaseStream.Seek(0, SeekOrigin.Begin);
-
-            // To read line from input stream
-            string textLine = sr.ReadLine();
-
-            // To read the whole file line by line
-            while (textLine != null)
-            {
-                Console.WriteLine(textLine);
-                textLine = sr.ReadLine();
-            }
-            Console.ReadLine();
-            sr.Close(); // Must close streamreader after use
-        }
-
+        //fil 1  customerdictionary  - innehåller 6 st key, id, username, isadmin, locked samt tillhörande värden
 
         //fil 2
         /// Customer
@@ -189,5 +165,33 @@ namespace KiwiBankomaten
         //if log is changed   write on file 3 and save
         // press to read accounts read on file 3
 
+
+
+
+
+        // Method to read and show data from chosen file.
+        //DataSaver.DataReading("File1");  HOW TO USE
+        public static void DataReading(string name)
+        {
+            // Takes in parameter which is the name of the file. Example, write "File1" 
+            string choice = @"C:\Users\danie\Desktop\" + name + ".txt";
+            StreamReader sr = new StreamReader($"{choice}");
+            Console.WriteLine($"Content of the File ({name}): \n");
+
+            // This is use to specify from where to start reading input stream
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            // To read line from input stream
+            string textLine = sr.ReadLine();
+
+            // To read the whole file line by line
+            while (textLine != null)
+            {
+                Console.WriteLine(textLine);
+                textLine = sr.ReadLine();
+            }
+            Console.ReadLine();
+            sr.Close(); // Must close streamreader after use
+        }
     }
 }
