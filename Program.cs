9e09991ea3da5963048418222c4df5276bda8477
PetterBostrom <petter.bostrom@gmail.com>
2022-12-14 12:33:09 +0100

    Added writing to files before exiting program.

--- Program.cs
+++ Program.cs
@@ -47,7 +47,11 @@ namespace KiwiBankomaten
                     
                     //Exit program
                     case "2":
-                        
+                        // Write all changes to files before exiting.
+                        foreach (string s in DataSaver.fileNames)
+                        {
+                            DataSaver.DSaver(s);
+                        }
                         //closes the program 
                         Environment.Exit(0);
                         break;
